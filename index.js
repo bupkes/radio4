d5eb722c61769beec802bf2eb19872f9bdb9e5c5
bupkes <po@bupk.es>
2017-02-01 22:40:16 +0000

    Added lots of JS

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 var request = require('request');
-
 request('http://www.bbc.co.uk/radio4/programmes/schedules/fm/today.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     body = JSON.parse(body);
-    var now = new Date();
-    var jsonDate = now.toJSON();
-    console.log(jsonDate);
-    var programmes = body.schedule.day.broadcasts;
-    console.log("try this:    " + programmes[1]);
+    function convertToTwoDigits(n) {return n > 9 ? "" + n : "0" + n;};
+
+
+    var listOfProgrammes = body.schedule.day.broadcasts;
+    listOfProgrammes.forEach(function(programme, index) {
+      // console.log(programme);
+
+      var date = new Date(Date.parse(programme.start))
+      var time = convertToTwoDigits(date.getUTCHours()) + "." + convertToTwoDigits(date.getUTCMinutes());
+      console.log(time + " - " + programme.programme.display_titles.title);
+
+    });
+
+
+
+
+
+
+    // function convertToTwoDigits(n) {return n > 9 ? "" + n : "0" + n;};
+    //
+    // var date = new Date(Date.parse(listOfProgrammes.start))
+    // var time = convertToTwoDigits(date.getUTCHours()) + "." + convertToTwoDigits(date.getUTCMinutes());
+    // console.log(time + " - " + listOfProgrammes.programme.programme.title);
 
 
 // go through each entry and check if the programme is on
@@ -16,3 +33,9 @@ request('http://www.bbc.co.uk/radio4/programmes/schedules/fm/today.json', functi
 
   }
 })
+
+
+// NOW
+// var now = new Date();
+// var jsonDate = now.toJSON();
+// console.log(jsonDate);
